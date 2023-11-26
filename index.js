c1d6b132c263d0274da444554f1f96bbc8071bc3
mizan-chowdhury <mizan1034@gmail.com>
2023-11-26 16:49:06 +0600

    created api for users and subscribers

--- index.js
+++ index.js
@@ -24,13 +24,36 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const galleryPhotos = client.db('dexterFitnessTrainer').collection('galleryPhotos')
-   
+    const subscribers = client.db('dexterFitnessTrainer').collection('subscribers')
+    const users = client.db('dexterFitnessTrainer').collection('users')
+    
+
+    // gallery photos api
     app.get('/photos', async(req,res)=>{
         const limit = parseInt(req.query.limit);
         const page = parseInt(req.query.page);
         const totalCount = await galleryPhotos.estimatedDocumentCount();
         const result = await galleryPhotos.find().skip(page).limit(limit).toArray();
         res.send({totalCount,result });
+    })
+
+    // subscribers api
+    app.post('/subscribers', async(req,res)=>{
+      const newSubscriber = req.body;
+      const result = await subscribers.insertOne(newSubscriber);
+      res.send(result);
+    })
+
+    // users api
+    app.post('/users', async(req,res)=>{
+      const newUser = req.body;
+      const query = {email : newUser.email}
+      const insertedUser = await users.findOne(query)
+      if(insertedUser){
+       return res.send({message: 'user already existed', insertedId: null})
+      }
+      const result = await users.insertOne(newUser);
+      res.send(result);
     })
 
     await client.db("admin").command({ ping: 1 });
