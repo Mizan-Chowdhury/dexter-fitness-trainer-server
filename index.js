07ee118842f738c69f5b12cf7d0af0b80ca9c722
mizan-chowdhury <mizan1034@gmail.com>
2023-11-26 18:54:27 +0600

    added the token verify middleware

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middlewares
-app.use(cors());
-app.use(express.json())
-
+app.use(cors({
+  origin : ['http://localhost:5173'],
+  credentials: true,
+  // optionsSuccessStatus: 200
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qzinma9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,59 +24,104 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const galleryPhotos = client.db('dexterFitnessTrainer').collection('galleryPhotos')
-    const subscribers = client.db('dexterFitnessTrainer').collection('subscribers')
-    const users = client.db('dexterFitnessTrainer').collection('users')
-    
+    // create secret token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
 
-    // gallery photos api
-    app.get('/photos', async(req,res)=>{
-        const limit = parseInt(req.query.limit);
-        const page = parseInt(req.query.page);
-        const totalCount = await galleryPhotos.estimatedDocumentCount();
-        const result = await galleryPhotos.find().skip(page).limit(limit).toArray();
-        res.send({totalCount,result });
+    // remove token
+    app.post('/logout', (req, res)=>{
+      res.clearCookie('token',{maxAge:0}).send({success:false})
     })
 
+    // token verify middleware
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies.token;
+      if (!token) {
+        return res.status(401).send({ error: "unAuthorized" });
+      }
+      jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ error: "unAuthorized" });
+        }
+        req.user = decoded;
+        next();
+      });
+    };
+
+    const galleryPhotos = client
+      .db("dexterFitnessTrainer")
+      .collection("galleryPhotos");
+    const subscribers = client
+      .db("dexterFitnessTrainer")
+      .collection("subscribers");
+    const users = client.db("dexterFitnessTrainer").collection("users");
+
+    // gallery photos api
+    app.get("/photos", async (req, res) => {
+      const limit = parseInt(req.query.limit);
+      const page = parseInt(req.query.page);
+      const totalCount = await galleryPhotos.estimatedDocumentCount();
+      const result = await galleryPhotos
+        .find()
+        .skip(page)
+        .limit(limit)
+        .toArray();
+      res.send({ totalCount, result });
+    });
+
     // subscribers api
-    app.post('/subscribers', async(req,res)=>{
+    app.get('/subscribers', async(req,res)=>{
+      const result = await subscribers.find().toArray();
+      res.send(result);
+    })
+    app.post("/subscribers", async (req, res) => {
       const newSubscriber = req.body;
       const result = await subscribers.insertOne(newSubscriber);
       res.send(result);
-    })
+    });
 
     // users api
-    app.post('/users', async(req,res)=>{
+    app.post("/users", async (req, res) => {
       const newUser = req.body;
-      const query = {email : newUser.email}
-      const insertedUser = await users.findOne(query)
-      if(insertedUser){
-       return res.send({message: 'user already existed', insertedId: null})
+      const query = { email: newUser.email };
+      const insertedUser = await users.findOne(query);
+      if (insertedUser) {
+        return res.send({ message: "user already existed", insertedId: null });
       }
       const result = await users.insertOne(newUser);
       res.send(result);
-    })
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", async (req, res) => {
+  res.send("server is running on UI");
+});
 
-
-
-app.get('/', async(req,res)=>{
-    res.send('server is running on UI');
-})
-
-app.listen(port, ()=>{
-    console.log('server is on port :', port);
-})
+app.listen(port, () => {
+  console.log("server is on port :", port);
+});
